6aef633ac4d799d48fc4a14eaed57d6d351ea85f
Gagan Nagar <gaganngr319@gmail.com>
2026-02-24 22:06:22 +0530

    main

--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ const app = express()
 const notes =  [];
 app.use(express.json())   /// ye ek middleware h kyuki express direct data access nhi kr skti isliye 
 
+
+
+//1....post method ka use karke hamne ek api create ki /notes jisme user ne data send krega create karne k liye 
+
 app.post('/notes', (req, res) =>{
     notes.push(req.body);  // data save and create karne k liye 
  console.log(req.body)
@@ -15,4 +19,50 @@ app.post('/notes', (req, res) =>{
     })
 })
 
+
+//2..get method ka use karke ham ek api create karenge jaha user server k through data fetch karvayega
+
+app.get('/notes', (req, res)=>{
+
+    res.status(200).json({
+        message: "notes fetched successfully",
+        notes:notes
+    })
+
+})
+
+
+
+///3....delete method ka use karke ham kisi note ko delete karenge using index ok
+
+app.delete('/notes/:index', (req, res)=>{
+
+    // sabse pehle ham req. ke andr jo data h usme se index nikalenge jo user ko dlt karna h
+    const index = req.params.index
+    delete notes[index]
+
+    res.status(200).json({
+        message: "Note deleted successfully"
+    })
+})
+
+
+//4...patch method ka use karke ham kisi note ko update karenge server par jo data h usko
+app.patch('/notes/:index', (req, res)=>{
+
+    const index = req.params.index;
+    const title = req.body.title;
+    const des = req.body.description
+
+    notes[index].title = title;
+    notes[index].description=des;
+
+    res.status(200).json({
+        message:"note updated successfully"
+    })
+})
+
+
+
 module.exports = app; // hamne export kiya server 
+
